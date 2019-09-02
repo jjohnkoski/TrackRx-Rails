2c254bdb08ef7e82494656287e35ce2ddccd7678
jjohnkoski <jacobjohnkoski@gmail.com>
2019-09-02 15:41:52 -0500

    Created detail feature.

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,4 +2,8 @@ class MedicationsController < ApplicationController
     def index
         @medications = Medicine.all
     end
+
+    def show
+        @medication = Medicine.find_by(name: params[:name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
     get "medications" => "medications#index"
+    get "medications/:name" => "medications#show"
 end
