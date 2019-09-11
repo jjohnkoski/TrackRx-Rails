e2ca629dfad81b7de4987f2614f02ed24b310532
jjohnkoski <jacobjohnkoski@gmail.com>
2019-09-11 16:38:15 -0500

    Added create and updated routes.

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -13,8 +13,23 @@ class MedicinesController < ApplicationController
 
     def update
         @medicine = Medicine.find(params[:id])
-        medicine_params = params.require(:medicine).permit!
         @medicine.update(medicine_params)
         redirect_to @medicine
+    end
+
+    def new
+        @medicine = Medicine.new
+    end
+
+    def create
+        @medicine = Medicine.new(medicine_params)
+        @medicine.save
+        redirect_to @medicine
+    end
+
+private
+
+    def medicine_params
+        params.require(:medicine).permit!
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
     root "medicines#index"
-    get "medicines" => "medicines#index"
-    get "medicines/:id" => "medicines#show", as: "medicine"
-    get "medicines/:id/edit" => "medicines#edit", as: "edit_medicine"
-    patch "medicines/:id" => "medicines#update"
+    resources :medicines
 end
