97dd8429044cdd3b315f8321ad34860e5e6a690f
jjohnkoski <jacobjohnkoski@gmail.com>
2019-09-30 17:25:47 -0500

    Fixed users/sessions needs fix.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        if user = User.authenticate(params[:email], params[:password])
+            session[:user_id] = user.id
+            flash[:notice] = 'Welcome back!'
+            redirect_to user
+        else
+            flash.now[:alert] = 'Invalid password or username.'
+            render :new
+        end
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     get 'signup' => 'users#new'
     resources :users
+    resource :session
     root "medicines#index"
     resources :medicines
 end
