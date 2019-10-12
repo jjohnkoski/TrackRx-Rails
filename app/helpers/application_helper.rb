760f36aded96eb6da3a14d48f361fb826f0614c3
jjohnkoski <jacobjohnkoski@gmail.com>
2019-10-12 10:43:53 -0500

    Tweaked action specific content layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def page_title
+        if content_for?(:title)
+            "TrackRx-#{content_for(:title)}"
+        else
+            "TrackRx"
+        end
+    end
 end
